8d50fb27aaca1f3744345b6b74469acc5e70003a
ilya-yalovoy <80987027+ilya-yalovoy@users.noreply.github.com>
2021-06-23 20:26:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,183 +1,95 @@
 var arr1 = [
     "./img/block1/1_0001.png",
-    
-    "./img/block1/1_0003.png",
-    
-    "./img/block1/1_0005.png",
-    
-    "./img/block1/1_0007.png",
-    
-    "./img/block1/1_0009.png",
-    
-    "./img/block1/1_0011.png",
-    
-    "./img/block1/1_0013.png",
-    
-    "./img/block1/1_0015.png",
-    
-    "./img/block1/1_0017.png",
-    
-    "./img/block1/1_0019.png",
-    
-    "./img/block1/1_0021.png",
-    
-    "./img/block1/1_0023.png",
-    
-    "./img/block1/1_0025.png",
-    
-    "./img/block1/1_0027.png",
-    
-    "./img/block1/1_0029.png",
-    
-    "./img/block1/1_0031.png",
-    
-    "./img/block1/1_0033.png",
-    
-    "./img/block1/1_0035.png",
-    
-    "./img/block1/1_0037.png",
-    
-    "./img/block1/1_0039.png",
-    
-    "./img/block1/1_0041.png",
-    
-    "./img/block1/1_0043.png",
-    
-    "./img/block1/1_0045.png",
-    
-    "./img/block1/1_0047.png",
-    
-    "./img/block1/1_0049.png",
-    
-    "./img/block1/1_0051.png",
-    
-    "./img/block1/1_0053.png",
-    
-    "./img/block1/1_0055.png",
-    
-    "./img/block1/1_0057.png",
-    
-    "./img/block1/1_0059.png",
-    
-    "./img/block1/1_0061.png",
-    
-    "./img/block1/1_0063.png",
-    
-    "./img/block1/1_0065.png",
-    
-    "./img/block1/1_0067.png",
-    
-    "./img/block1/1_0069.png",
-    
-    "./img/block1/1_0071.png",
-    
-    "./img/block1/1_0073.png",
-    
-    "./img/block1/1_0075.png",
-    
-    "./img/block1/1_0077.png",
-    
-    "./img/block1/1_0079.png",
-    
-    "./img/block1/1_0081.png",
-    
-    "./img/block1/1_0083.png",
-    
-    "./img/block1/1_0085.png",
-    
-    "./img/block1/1_0087.png",
-    
-    "./img/block1/1_0089.png",
-    
-    "./img/block1/1_0091.png",
-    
-    "./img/block1/1_0093.png",
-    
-    "./img/block1/1_0095.png",
-    
-    "./img/block1/1_0097.png",
-    
-    "./img/block1/1_0099.png",
-    
-    "./img/block1/1_0102.png",
-    
-    "./img/block1/1_0104.png",
-    
-    "./img/block1/1_0106.png",
-    
-    "./img/block1/1_0108.png",
-    
-    "./img/block1/1_0110.png",
-    
-    "./img/block1/1_0112.png",
-    
-    "./img/block1/1_0114.png",
-    
-    "./img/block1/1_0116.png",
-    
-    "./img/block1/1_0118.png",
-    
-    "./img/block1/1_0120.png",
-    
-    "./img/block1/1_0122.png",
-    
-    "./img/block1/1_0124.png",
-    
-    "./img/block1/1_0126.png",
-    
-    "./img/block1/1_0128.png",
-    
-    "./img/block1/1_0130.png",
-    
-    "./img/block1/1_0132.png",
-    
-    "./img/block1/1_0134.png",
-    
-    "./img/block1/1_0136.png",
-    
-    "./img/block1/1_0138.png",
-    
-    "./img/block1/1_0140.png",
-    
-    "./img/block1/1_0142.png",
-    
-    "./img/block1/1_0144.png",
-    
-    "./img/block1/1_0146.png",
-    
-    "./img/block1/1_0148.png",
-    
-    "./img/block1/1_0150.png",
-    
-    "./img/block1/1_0152.png",
-    
-    "./img/block1/1_0154.png",
-    
-    "./img/block1/1_0156.png",
-    
-    "./img/block1/1_0158.png",
-    
-    "./img/block1/1_0160.png",
-    
-    "./img/block1/1_0162.png",
-    
-    "./img/block1/1_0164.png",
-    
-    "./img/block1/1_0166.png",
-    
-    "./img/block1/1_0168.png",
-    
-    "./img/block1/1_0170.png",
-    
-    "./img/block1/1_0172.png",
-    
-    "./img/block1/1_0174.png",
-    
-    "./img/block1/1_0176.png",
-    
-    "./img/block1/1_0178.png",
-    
-    "./img/block1/1_0180.png"
+"./img/block1/1_0003.png",
+"./img/block1/1_0005.png",
+"./img/block1/1_0007.png",
+"./img/block1/1_0009.png",
+"./img/block1/1_0011.png",
+"./img/block1/1_0013.png",
+"./img/block1/1_0015.png",
+"./img/block1/1_0017.png",
+"./img/block1/1_0019.png",
+"./img/block1/1_0021.png",
+"./img/block1/1_0023.png",
+"./img/block1/1_0025.png",
+"./img/block1/1_0027.png",
+"./img/block1/1_0029.png",
+"./img/block1/1_0031.png",
+"./img/block1/1_0033.png",
+"./img/block1/1_0035.png",
+"./img/block1/1_0037.png",
+"./img/block1/1_0039.png",
+"./img/block1/1_0041.png",
+"./img/block1/1_0043.png",
+"./img/block1/1_0045.png",
+"./img/block1/1_0047.png",
+"./img/block1/1_0049.png",
+"./img/block1/1_0051.png",
+"./img/block1/1_0053.png",
+"./img/block1/1_0055.png",
+"./img/block1/1_0057.png",
+"./img/block1/1_0059.png",
+"./img/block1/1_0061.png",
+"./img/block1/1_0063.png",
+"./img/block1/1_0065.png",
+"./img/block1/1_0067.png",
+"./img/block1/1_0069.png",
+"./img/block1/1_0071.png",
+"./img/block1/1_0073.png",
+"./img/block1/1_0075.png",
+"./img/block1/1_0077.png",
+"./img/block1/1_0079.png",
+"./img/block1/1_0081.png",
+"./img/block1/1_0083.png",
+"./img/block1/1_0085.png",
+"./img/block1/1_0087.png",
+"./img/block1/1_0089.png",
+"./img/block1/1_0091.png",
+"./img/block1/1_0093.png",
+"./img/block1/1_0095.png",
+"./img/block1/1_0097.png",
+"./img/block1/1_0099.png",
+"./img/block1/1_0101.png",
+"./img/block1/1_0103.png",
+"./img/block1/1_0105.png",
+"./img/block1/1_0107.png",
+"./img/block1/1_0109.png",
+"./img/block1/1_0111.png",
+"./img/block1/1_0113.png",
+"./img/block1/1_0115.png",
+"./img/block1/1_0117.png",
+"./img/block1/1_0119.png",
+"./img/block1/1_0121.png",
+"./img/block1/1_0123.png",
+"./img/block1/1_0125.png",
+"./img/block1/1_0127.png",
+"./img/block1/1_0129.png",
+"./img/block1/1_0131.png",
+"./img/block1/1_0133.png",
+"./img/block1/1_0135.png",
+"./img/block1/1_0137.png",
+"./img/block1/1_0139.png",
+"./img/block1/1_0141.png",
+"./img/block1/1_0143.png",
+"./img/block1/1_0145.png",
+"./img/block1/1_0147.png",
+"./img/block1/1_0149.png",
+"./img/block1/1_0151.png",
+"./img/block1/1_0153.png",
+"./img/block1/1_0155.png",
+"./img/block1/1_0157.png",
+"./img/block1/1_0159.png",
+"./img/block1/1_0161.png",
+"./img/block1/1_0163.png",
+"./img/block1/1_0165.png",
+"./img/block1/1_0167.png",
+"./img/block1/1_0169.png",
+"./img/block1/1_0171.png",
+"./img/block1/1_0173.png",
+"./img/block1/1_0175.png",
+"./img/block1/1_0177.png",
+"./img/block1/1_0179.png",
+"./img/block1/1_0180.png"
         ];
 var arr2 = [
     "./img/block2/1_0025.png",
@@ -270,7 +182,13 @@ var main   = document.querySelector('.wrapper'),
     activeSlideAnim = 0,
     activeBlock = 0;
 
-
+    window.onload = function () {
+        document.body.classList.add('loaded_hiding');
+        window.setTimeout(function () {
+          document.body.classList.add('loaded');
+          document.body.classList.remove('loaded_hiding');
+          b1Content.classList.add('header__content-active');
+        }, 500);}
 
 var objMain = {
     //STATIC
@@ -306,9 +224,6 @@ function Down() {
                 //console.log(activeSlideAnim);
                 b1Anim.style.backgroundImage = 'url("' + arr1[activeSlideAnim] + '")';
                 
-            }
-            if (activeSlideAnim>43 && activeSlideAnim< 48) {
-                b1Content.classList.add('header__content-active');
             }
             if (activeSlideAnim >= 89) {
                 activeSlideAnim = 0;
@@ -350,9 +265,6 @@ function Up() {
                 //console.log('url("' + arr1[activeSlideAnim] + '")');
                 b1Anim.style.backgroundImage = 'url("' + arr1[activeSlideAnim] + '")';
                 
-            }
-            if (activeSlideAnim>43 && activeSlideAnim< 48) {
-                b1Content.classList.remove('header__content-active');
             }
             if (last_scroll == 0) {
                 activeSlideAnim = 0;
